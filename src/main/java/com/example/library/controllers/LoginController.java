6c560a439035d910f38138cc17c215684f1aa6de
Viktor Yordanov <viktor.yordanov@sportal.com>
2023-05-26 16:24:30 +0300

    Add email validation on Login screen

--- src/main/java/com/example/library/controllers/LoginController.java
+++ src/main/java/com/example/library/controllers/LoginController.java
@@ -3,6 +3,8 @@ package com.example.library.controllers;
 import com.example.library.MainApplication;
 import com.example.library.dao.UserDao;
 import com.example.library.entities.User;
+import com.example.library.errors.ErrorMessages;
+import com.example.library.exceptions.ValidationInputException;
 import com.example.library.helpers.HashPasswordHelper;
 import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
@@ -23,11 +25,19 @@ public class LoginController {
   private Label wrongCredentials;
 
   @FXML
-  protected void onLoginButtonClick() throws NoSuchAlgorithmException {
+  protected void onLoginButtonClick() throws NoSuchAlgorithmException, ValidationInputException {
     validateLogin();
   }
 
-  public void validateLogin() throws NoSuchAlgorithmException {
+  public void validateLogin() throws NoSuchAlgorithmException, ValidationInputException {
+    String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    if (!emailInput.getText().matches(emailRegex)) {
+      wrongCredentials.setText(
+          ErrorMessages.invalidEmail);
+      throw new ValidationInputException(ErrorMessages.invalidEmail);
+    }
+
     String cryptedPassword = HashPasswordHelper.hashPassword(passwordInput.getText());
     Optional<User> newUser = new UserDao().findUser(emailInput.getText(), cryptedPassword);
 
